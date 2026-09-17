f3c30744ff7ceaa2cb648dd482dae866cf37bfbe
HJC <aah.97130@gmail.com>
2026-09-17 11:00:29 +0800

    <2>

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello world")
+temperature =25.3
+sensor ="TEMP-01"
+count=8
+print(sensor)
+print(temperature)
---
+++ w2.py
@@ -0,0 +1,7 @@
+temp=25.0
+print(temp)
+temp=26.5
+print(temp)
+count = 0
+count =count +1
+print(count)
