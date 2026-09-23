de3e8539490eba1da20d9e95eccde031e524b121
HJC <aah.97130@gmail.com>
2026-09-23 19:15:07 +0800

    923確認

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-temperature =25.3
+temperature =25.35
 sensor ="TEMP-01"
 count=8
 print(sensor)
--- w2.py
+++ w2.py
@@ -3,5 +3,36 @@ print(temp)
 temp=26.5
 print(temp)
 count = 0
-count =count +1
+count =count +10
+a="NULL"
 print(count)
+print(type(count))
+print(type(a))
+print(type(temp))
+
+# 資料型態轉換
+str1="ID"+"520"
+str2="123"+str1
+print(str1)
+test="第一"+str(count)+"筆"
+print(test)
+print(str2)
+#應用
+reading="23.7 "
+print(reading*2)
+value = float(reading)
+print(f"溫度{value}度")
+print(f"兩倍 {value * 2} 度")#要f{}才是變數
+#隨堂練習
+sensorID="reading"
+temprature="30.5"
+count=1
+print(sensorID )
+print(temprature)
+print(count)
+print(type(sensorID) )
+print(type(temprature))
+print(type(count))
+a=float(temprature)+0.5
+print( sensorID +"=" + str(a) )
+
